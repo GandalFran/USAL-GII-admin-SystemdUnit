d64f9eb10401977a5595b0605e481d8af56879bc
Francisco Pinto Santos <franpintosantos@usal.es>
2019-03-04 16:08:08 +0100

    final version of scripts

--- cockroach.sh
+++ cockroach.sh
@@ -10,37 +10,37 @@ FA4=349.23
 SOL4=392
 LA4=440
 
-CORCHEA=100
+CORCHEA=200
 NEGRA=$CORCHEA*2
 BLANCA=$NEGRA*2
 SEMICORCHEA=125
 
 DELAY=100
 
-sing_stop(){
+sing_cucaracha(){
 	beep   -f $SOL3 -l $CORCHEA					\
-		-n -f $SOL3 -l $SEMICORCHEA 			\
+		-n -f $SOL3 -l $SEMICORCHEA 				\
 		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $DO4 -l $NEGRA					\
 		-n -f $MI4 -l $SEMICORCHEA				\
 		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA -D $DELAY	\
+		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $DO4 -l $NEGRA					\
 		-n -f $MI4 -l $BLANCA					\
-		-n -f $DO4 -l $SEMICORCHEA -D $DELAY	\
+		-n -f $DO4 -l $SEMICORCHEA				\
 	 	-n -f $DO4 -l $SEMICORCHEA				\
 		-n -f $SI3 -l $SEMICORCHEA				\
 		-n -f $SI3 -l $SEMICORCHEA				\
 		-n -f $LA3 -l $SEMICORCHEA				\
 		-n -f $LA3 -l $SEMICORCHEA				\
 		-n -f $SOL3 -l $NEGRA					\
-		-n -f $SOL3 -l $CORCHEA -D $DELAY		\
+		-n -f $SOL3 -l $CORCHEA					\
 		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $SI3 -l $NEGRA					\
 		-n -f $RE4 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA -D $DELAY	\
+		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $SOL3 -l $SEMICORCHEA				\
 		-n -f $SI3 -l $NEGRA					\
@@ -54,4 +54,10 @@ sing_stop(){
 		-n -f $DO4 -l $BLANCA
 }
 
+eject
+eject -t
+eject
 sing_cucaracha
+eject -t
+eject
+eject -t
--- installunit.sh
+++ installunit.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
 #########################################################################################
+PROGRAM_NAME="cockroach.sh"
+PROGRAM_FINAL_PATH="/bin/cockroach.sh"
+
 SERVICE_NAME="cockroach"
-COMMAND="sh /bin/scripts/cockroach.sh"
+COMMAND="/bin/sh $PROGRAM_FINAL_PATH"
+
 SERVICE_DESCRIPTION="Script to play \"La cucaracha\" with beep at the start of session"
-TARGET_DEPENDENCES=""
+TARGET_DEPENDENCES="getty.target"
 START_AT_RUNLEVEL="multi-user.target"
 ##########################################################################################
 
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 SERVICE_DECLARATION="
-
-[Unit]
-Description=$SERVICE_DESCRIPTION
-After=$TARGET_DEPENDENCES
-StartLimitIntervalSec=0
-
-[Service]
-Type=simple
-Restart=always
-User=root
-ExecStart=$COMMAND
-
-[Install]
-WantedBy=$START_AT_RUNLEVEL
+\n[Unit]
+\nDescription=$SERVICE_DESCRIPTION
+\nAfter=$TARGET_DEPENDENCES
+\nStartLimitIntervalSec=0
+\n
+\n[Service]
+\nType=simple
+\nUser=root
+\nExecStart=$COMMAND
+\n
+\n[Install]
+\nWantedBy=$START_AT_RUNLEVEL
 "
 
+#Copy the file to the final destination
+cp $PROGRAM_NAME $PROGRAM_FINAL_PATH
 #Create service description file
 echo $SERVICE_DECLARATION >> $SERVICE_FILE
 #Start and prepare the service to start each time the session starts
+systemctl enable "$SERVICE_FILE"
 systemctl start "$SERVICE_NAME.service"
-systemctl --now enable "$SERVICE_NAME.service"
 
