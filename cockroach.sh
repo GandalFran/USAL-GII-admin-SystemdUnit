c4d1f2cdd0080a2cd692b538b0adb88c486fc678
Francisco Pinto Santos <franpintosantos@usal.es>
2019-03-05 12:37:42 +0100

    minor changes

--- cockroach.sh
+++ cockroach.sh
@@ -13,48 +13,45 @@ LA4=440
 CORCHEA=200
 NEGRA=$CORCHEA*2
 BLANCA=$NEGRA*2
-SEMICORCHEA=125
-
-DELAY=100
+SEMICORCHEA=$CORCHEA/2
 
 sing_cucaracha(){
-	beep   -f $SOL3 -l $CORCHEA					\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $DO4 -l $NEGRA					\
-		-n -f $MI4 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $DO4 -l $NEGRA					\
-		-n -f $MI4 -l $BLANCA					\
-		-n -f $DO4 -l $SEMICORCHEA				\
-	 	-n -f $DO4 -l $SEMICORCHEA				\
-		-n -f $SI3 -l $SEMICORCHEA				\
-		-n -f $SI3 -l $SEMICORCHEA				\
-		-n -f $LA3 -l $SEMICORCHEA				\
-		-n -f $LA3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $NEGRA					\
-		-n -f $SOL3 -l $CORCHEA					\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SI3 -l $NEGRA					\
-		-n -f $RE4 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SOL3 -l $SEMICORCHEA				\
-		-n -f $SI3 -l $NEGRA					\
-		-n -f $RE4 -l $BLANCA					\
-		-n -f $SOL4 -l $SEMICORCHEA				\
-		-n -f $LA4 -l $SEMICORCHEA				\
-		-n -f $SOL4 -l $SEMICORCHEA				\
-		-n -f $FA4 -l $SEMICORCHEA				\
-		-n -f $MI4 -l $SEMICORCHEA				\
-		-n -f $RE4 -l $SEMICORCHEA				\
-		-n -f $DO4 -l $BLANCA
+	beep -f $SOL3   -l $CORCHEA		\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $DO4  -l $NEGRA		\
+		-n -f $MI4  -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $DO4  -l $NEGRA		\
+		-n -f $MI4  -l $BLANCA		\
+		-n -f $DO4  -l $SEMICORCHEA	\
+	 	-n -f $DO4  -l $SEMICORCHEA	\
+		-n -f $SI3  -l $SEMICORCHEA	\
+		-n -f $SI3  -l $SEMICORCHEA	\
+		-n -f $LA3  -l $SEMICORCHEA	\
+		-n -f $LA3  -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $NEGRA		\
+		-n -f $SOL3 -l $CORCHEA		\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SI3  -l $NEGRA		\
+		-n -f $RE4  -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SOL3 -l $SEMICORCHEA	\
+		-n -f $SI3  -l $NEGRA		\
+		-n -f $RE4  -l $BLANCA		\
+		-n -f $SOL4 -l $SEMICORCHEA	\
+		-n -f $LA4  -l $SEMICORCHEA	\
+		-n -f $SOL4 -l $SEMICORCHEA	\
+		-n -f $FA4  -l $SEMICORCHEA	\
+		-n -f $MI4  -l $SEMICORCHEA	\
+		-n -f $RE4  -l $SEMICORCHEA	\
+		-n -f $DO4  -l $BLANCA
 }
 
 eject
 sing_cucaracha
 eject -t
-
